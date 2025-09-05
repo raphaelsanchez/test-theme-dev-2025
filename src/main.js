dfa5fe09670407d9efd7145054f81a3baaece9ef
Raphael Sanchez <hello@raphaelsanchez.design>
2025-09-05 16:12:42 +0200

    feat: implement mobile menu functionality and accessibility improvements

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-import './style.css'
+import "./style.css";
+import { initMobileMenu } from "./mobileMenu.js";
+
+/**
+ * Initialize mobile menu
+ *
+ * @param {number | undefined} breakpoint - The breakpoint to use for the mobile menu
+ * @returns {void}
+ */
+initMobileMenu();
